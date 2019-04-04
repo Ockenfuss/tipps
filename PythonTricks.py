7f089cb7ef767317abd5d941addc6c10e411ae50
Paul Ockenfuss <paul.ockenfuss@campus.lmu.de>
2019-04-05 01:15:49 +0200

    My personal overview files

---
+++ PythonTricks.py
@@ -0,0 +1,209 @@
+Install:
+sudo apt install python3
+Pip: sudo apt install python3-Pip   
+import numpy as np
+
+
+
+
+#Verschiedenste Tricks im Umgang mit Python
+################################Allgemein#################################
+sys.exit()#beendet ein Python Program #Stop #Interrupt
+
+#Datentypen
+int(x)#convert to integer
+
+#################################Arrays###################################
+for index, values in enumerate(array, start)#returns the position as well as the value of the array
+#numpy.ndenumerate: Extremely useful for looping over multidimensional arrays! Gives index as tuple.
+#if index should be single value: enumerate(array.flatten())
+
+np.atleast_1d(arrays)#Converts scalars to array and leaves everything else untouched=>Usefull for functions to handle scalars as well arrays
+np.atleast_2d(array)#convert to 2d by "adding one pair of brackets": array2d=[array1d] or array2d=[[scalar]]
+############################Dateipfade########################paths#file
+import os.path
+
+############################Datetime module##############################
+#Datum und Zeit
+#Vergleiche: PlotSeries.py
+import datetime
+from dateutil.parser import parse
+#Datum aus String: strptime()
+time = np.array([datetime.datetime.strptime(TimeString[i],"%m.%d.%y %H:%M:%S") for i in range(len(TimeString))])
+"%m.%d.%y %H:%M:%S.%f"#falls mit Milliseconds
+#String aus Datum: strftime()
+s=time.strftime("%H:%M")
+#Differenz in Sekunden: (a-b).total_seconds()
+t=np.array([(time[i]-time[i+1]).total_seconds() for i in range(len(time))])
+#Zeiten addieren:
+time=time+timedelta(hours=7)
+
+#Zeiten Plotten: "time" muss dabei wieder ein Array aus datetime-Objekten sein.
+#Vergleiche: PlotJPLResults.py
+import matplotlib.dates as dates
+ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
+#Wichtig: Wenn Daten außer einer Uhrzeit auch ein Datum enthalten, muss dieses in den Limits auch angegeben werden.
+ax.set_xlim(datetime.datetime.strptime("8.21.2017 15:00","%m.%d.%y %H:%M:%S"),datetime.datetime.strptime("8.21.2017 16:00","%m.%d.%y %H:%M:%S"))
+#Abstände der Labels (hier 2 Minuten)
+ax.xaxis.set_major_locator(dates.MinuteLocator(interval=2))
+
+
+plt.plot(time,d)
+
+
+
+
+############time Module######
+import time
+#Pause
+time.sleep(5.5)#5.5 seconds pause
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################Numpy##########################################################
+#Numpy Arrays erstellen
+np.zeros(100)
+np.ones(100)
+xgrid, ygrid=np.meshgrid(x,y)#make e.g. two 2D grids of two 1D arrays
+
+#Boolsche Indizes: auswählen eines bestimmten Teilarrays
+#<,>,== geht direct, für Verknüpfung (Oder, Und) mehrerer Ausdrücke:
+b=np.logical_and(t<deltat, t>=0)
+print(t[b])
+#Elementwise boolean operations: https://docs.scipy.org/doc/numpy-1.13.0/reference/routines.logic.html
+np.logical_and(a,b)#perform logical and elementwise
+
+
+#Numpy sortieren
+#argsort: Liefert array mit den Indizes in der sortierten Reihenfolge => einsetzen liefert sortiertes Array
+ind=np.argsort(a)
+print(a[ind])#sortiert
+
+
+#Numpy Elemente finden: np.where gibt array zurück mit allen reihenindizes in einem array und allen columnindizes in zweitem.Bsp:
+a=[1,1,1,2,2,3]
+indexarray=np.where(a==1)#gibt array: (array([0, 1, 2]),)=> indexarray[0] ist also [0,1,2] für die vorkommen von "1" in a
+
+#Numpy Array reshape
+#Imagine the old array to be flattened first: First, the last dimension is counted up (0,1,2,3...), then the second last is raised +1 and the last counted up again and so on.
+#Secondly, the resulting 1d array is filled into the new one by the same principle: the first (50) elements of the 1D array are put in the last dimension (0-50), then the second last dimension (0-400) is raised +1 and the next (50) elements are put into the last dimension (0-50) again, and so on.
+new=old.reshape((3,400,50))
+
+#Numpy Datentypen
+arr2=arr1.astype(int)#Array conversion
+
+#Numpy Funktionen
+np.mean(Array, Axis=0)#Numpy
+
+np.arctan2(y,x)#Gives the angle of a point x,y including information about the quadrant (be aware: arctan2(y,x), NOT x,y!)
+#x=1, y=1: 45°, x=-1, y=1: 135°, x=-1, y=-1: -135°, x=1, y=-1: -45° (upper half (y>0) positive angles, lower half negative angles)
+############################################Pyplot/Matplotlib###############################################################################################
+#https://matplotlib.org/faq/usage_faq.html#usage
+#Basic structure: A Figure object is the empty window which contains the plots.
+#In the figure is a certain number of Axes objects, the actual "plots".
+#Each Axes object can have for example two or three Axis-objects, the actual "axes of the plots". Axes!=Axis!!!
+
+###create plot
+fig,ax=plt.subplots()
+
+###Axen und Ticks
+ax.set_ylim(1e-7,5e1)#Limits
+ax.set_xticks([1,2,3])#Ticks setzen. ax.get_xticks() liefert ticks
+ax.tick_params(labelsize=16)
+
+ax2.set(ylabel="ratio", title="Titel")#Beschriftung
+ax.grid(True, which='major')#Gitter
+###Beschriftung mit Latex
+plt.rc('text', usetex=True)
+ax1.set_xlabel(r'Irradiance [$\frac{mW}{m^{2}nm}]$', fontsize=18)
+ax.plot(x,y, label=r'$\phi={0}\pi$'.format(i))#use variable in latex label
+
+###Legende
+legend2 = ax2.legend(loc='lower right', shadow=True, fontsize='medium')#Legende
+dummy_lines = []#Legende nur mit Linestyle
+dummy_lines.append(ax2.plot([],[], c="black", linestyle ="-", linewidth=1.2)[0])
+dummy_lines.append(ax2.plot([],[], c="black", linestyle ="--", linewidth=1.6)[0])
+legend2 = ax2.legend([dummy_lines[i] for i in [0,1]], ["Measurements", "Simulation"], loc="upper left")
+ax2.add_artist(legend2)
+
+#Color cycle setzen
+cm = plt.get_cmap('gist_rainbow')
+ax.set_prop_cycle(plt.cycler('color', [cm(1.*i/15) for i in range(15)]))
+#linestyle cycle
+from itertools import cycle
+lines = ["-","--","-.",":"]
+linecycler = cycle(lines)
+ax.plot(x,y, linestyle=next(linecycler))
+
+###colorbar: They always live in their own axes object!
+cbar=fig.colorbar(im)#If there are multiple axes and we want to assign a colorbar to one, we can create a small axes next to the axes with the image:
+cbar.set_label("Label")
+cbar.ax.tick_params(labelsize=10)
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+divider = make_axes_locatable(ax[0])#If there are multiple axes 0,1,2,3...
+cax = divider.append_axes('right', size='5%', pad=0.05)
+fig.colorbar(contour, cax=cax)
+
+###Zweite Axe rechts:
+ax2=ax.twinx()#twiny() for axis on top
+
+###Figuren
+ax.axvline(x=20)#axhline(y=20)
+#Kreis plotten
+circle1 = plt.Circle((1000, 1000), 30, color='r', fill=True)
+ax.add_artist(circle1)
+
+###Save File Location default and default extension (pdf)
+plt.rcParams["savefig.directory"] =os.path.dirname(os.path.abspath(__file__))#need to "import os"
+plt.rcParams["savefig.format"]="pdf"
+
+###Size of plots
+fig = plt.figure(figsize=(4,3))#Define grid and specify over how many cells axes spread
+grid = plt.GridSpec(4, 3, hspace=0.4, wspace=0.2)
+ax1 = fig.add_subplot(grid[:-1,:])
+ax2 = fig.add_subplot(grid[-1, :])
+
+###############Plot types#############
+ax2.hist(distance,bins=100, weights=values, range=(-60,60))#histogramm
+ax1.text(1,2,"Hallo", rotation=45)#Annotation, Text
+
+###############Images (2D Verteilung) plotten#################
+from matplotlib.colors import LogNorm#falls mit LogNorm
+im=ax.imshow(b, cmap='gray', interpolation='none', norm=LogNorm())#b: 2D Array mit Pixelwerten
+im.cmap.set_under('k',1.) 
+
+
+#######################Animationen########################
+import matplotlib.animation as animation
+fig, ax=plt.subplots(2,2)
+ims=[]
+for i in range(1,200):
+    im=ax[0,1].imshow(image[i], cmap="Greys_r")
+    ims.append([im])
+ani = animation.ArtistAnimation(fig, ims, interval=30, blit=True, repeat_delay=40)
+ani.save('SimpleBox.mp4')
+plt.show()
+
+
+#############################Image processing#####################################
+#Convolution
+from scipy import signal as sig
+AcM=sig.convolve2d(A,M, mode='full', boundary='fill', fillvalue=0)
+#boundary: How boundary conditions are treated: fill missing values on the rim with a value, use periodic boundary conditions ('wrap') or mirror the values directly at the rim to the outside ('symm')
+#mode: Size of the resulting array, 'full', "valid", or "same"
+
+
+############################################Create your own modules#################################
+#https://www.digitalocean.com/community/tutorials/how-to-write-modules-in-python-3
+#All about paths and importing from other directories
+https://chrisyeh96.github.io/2017/08/08/definitive-guide-python-imports.html
---
+++ RTipps.r
@@ -0,0 +1,225 @@
+#Tipps zum Umgang mit R
+https://www.rstudio.com/resources/cheatsheets/ #Cheatsheets
+http://adv-r.had.co.nz/ #Advanced topics like memory management, functional programming, etc. 
+##########Datentypen######################################
+#Typbestimmung
+class(c(1,2,3))
+
+#Faktoren: Bestimmte häufig gebrauchte Merkmale (z.B. male/female). Achtung: !=Strings!!
+#Faktor zu String konvertieren:
+v=factor(c("l", "n", "n", "i", "l"), levels = c("n", "l", "i"))
+levels(v)[v[1]]#Idee: levels gibt faktoren als strings zurück, suche daraus richtigen aus
+mapvalues(x, from=c("a", "b"), to=c("A","B"))#library(plyr): ändert Faktornamen=> praktisch um in data frames zusätzliche 1:1 Beziehungen zu speichern, z.B. Ortsname und eindeutige zugehörige Abkürzung
+
+#Ausgabeformat von Zahlen als Strings
+options(OutDec= ".")
+
+#Lists
+#Leere Liste:
+list()
+
+names(list)#Namen in List 
+list[c("a", "b")]#gibt Liste mit einelementigen Sublisten mit den Werten von "a" und "b"
+unlist(list[c("a","b")])#Vektor mit den Elementen aus "a" und "b" in "list" (i.A.: "unlist" löst alle Sublisten auf und fügt den Inhalt anschließend zu einem Vektor zusammen)
+
+#Tables
+t=table(c(0.7,0.8,0.8,0.9))#Erstellt Tabelle mit Häufigkeiten (eig. named vector)
+t[[2]]#=2 da 0.8 zweimal
+names(t)
+
+#Vektor
+#Leerer Vektor:
+v=vector()
+#Add elements:
+v=c(v,1:10)
+#konstanter wert 1
+rep(1,10000)
+#Equally spaced points
+seq(1,10)#optional: increment by=0.5 or number of points length.out=20 | Includes start and endpoint
+seq_along(c(1,55,2,60,9,4))#gives [1,2,3,4,5,6] vektor of all indices, useful for for-loops
+
+#Matrix
+matrix("test", nrow=3, ncol=3)
+
+#Daten: werden von 1970 aus gemessen
+#Zahl zu Datum:
+as.Date(1234, origin="1970-01-01")
+#String zu Datum
+as.Date("05/27/84","%m/%d/%y")#https://www.r-bloggers.com/date-formats-in-r/ Übersicht formatter
+#Datum zu String
+format(myDate, "%a %b %d")
+#Abstand in Tagen
+as.numeric(as.Date(date1, "%m/%d/%y")-as.Date(origin,"%m/%d/%y"))
+#########################Finden&Vergleichen##############################################
+#Element in Vektor/List/...
+match(x, vektor)#returns position of first occurence
+x %in% vektor #true or false alternativ: is.element(x,vektor)
+which.min(vec)#Index of Minimum
+
+duplicated(vektor)#logical vector with TRUE if element is duplicated
+unique(vektor)#vector with doubled rows removed
+############################Sortieren#########################################
+#Order: gibt die reihenfolge der indizes des originalvektors, damit dieser sortiert wäre
+order(v)=>v[order(v)] sortiert v!
+dat[order(dat$v),]#sortiers data frame rows anhand von column "v"
+
+###################Nützliche Funktionen###########################################
+#Differenzvektor: Länge n-1, berechnet differenz benachbarter Elemente v[i]-v[i-1]
+diff(v)
+#Integral über diskrete Funktionswerte y an den Stellen x
+pracma::trapz(x,y)
+lsf.str("package:dplyr") #list all functions in package
+
+#check if variable is of certain value (NULL or NaN or Inf/-Inf)
+is.null(x) is.finite(x), is.infinite(x), is.nan(x)
+rev(vec)#revert order of vector
+##############################Curve Fits##################################################
+#least square fit:
+model=nls(ET~a*sin(b*day+c)+d, data=Data, start=list(a=0.5, b=0.02, c=-200, d=0.5))
+coef(model)
+
+
+#################################Apply################################################
+apply(variable, margin, function)#margin: 1 by row, 2 by column, 1:2 for each element in variable
+do.call(what, args)#Problem: Funktion, Argumente als Liste vorhanden=>what: function or string with function name args: List of arguments, names of List elements gives the names of the function arguments
+
+#########################Data Frames####################################################
+#Colnames (Analog: Rownames) setzen/lesen:
+colnames(Dataframe)=c("Hallo", "zweitens")
+make.names(names, unique=FALSE)#Erzeugt syntaktisch korrekte namen (keine Zahl am Anfang, eindeutig,...)
+#Strings nicht zu Faktoren konvertieren:
+data.frame(A=c("Hallo", "zweitens"), stringsAsFactors=FALSE)
+
+
+#-----------------------Joining------------------------------------------------------------#
+#merge(): http://www.datasciencemadesimple.com/join-in-r-merge-in-r/ 
+#Idee: Vereine zwei Dataframes anhand einer gemeinsamen Spalte, z.B. "time": Reihen, welche in dieser Spalte gleiche, eindeutige Werte haben, werden im vereinten Dataframe in eine Reihe gesetzt.
+#Sofern Werte doppelt vorkommen, werden im neuen Dataframe alle möglichen Kombinationen gelistet
+#Optionen: all=TRUE: keine Information geht verloren, Zellen,  die im neuen Dataframe nicht definiert sind, werden mit NA gefüllt
+#all.x=TRUE oder all.y=TRUE: nur die "Zeiten" aus dem ersten (x) bzw. zweiten (y) DataFrame werden im neuen gelistet
+#Keine Optionen (alle False): "Inner join", nur Reihen mit Werten, welche in der by-Spalte in beiden DataFrames vorkommen, werden im neuen DataFrame gelistet.
+merge(x=d1, y=d2, by="Time", all=TRUE)#by kann auch mehrere columns beinhalten!
+
+#Einfaches verbinden der Spalten (müssen gleiche Anzahl an Reihen haben!)/ Verbinden der Reihen (müssen gleiche Variablennamen haben!)
+cbind(d1,d2)
+rbind(d1,d2)
+######################Strings#############################################################
+#Regex: Replacement mit sub(), gsub() (letzteres ersetzt alle vorkommen)
+sub(pattern, replacement, text)
+grep(pattern, x)#return all indizes in x where pattern matched the string
+str_extract(text,pattern)#library(stringr): return the part of the text, which matched the pattern 
+=> pattern=paste(patternlist, collapse="|")# when multiple patterns are to be matched
+######################Boolean###################################################
+#Operators: single (& or |) perform vectorized comparison, double (&& or ||) compare only first elements of vectors
+####################################In- and Output##########################################
+#####Input#####
+read.table(file, sep=",", dec=".", stringsAsFactor=FALSE, comment.char="")#Beachte: leere Zellen (",,") werden sowohl als "" als auch als NA dargestellt, je nach Formatierung der Spalte
+#####Output####
+writeLines(text, con = stdout(), sep = "\n", useBytes = FALSE)#con: z.B. String mit Filename
+write.table(x=data.frame or matrix, file="", append=FALSE, sep=" ", dec=".", row.names = TRUE, col.names = TRUE)
+write.csv(x=data.frame, file="...", )
+#colored text output: https://github.com/r-lib/crayon
+####################Working with Files##############################################
+basename(path)
+dirname(path)
+list.files(path=".", pattern="abc")#pattern: regex pattern
+########################Tidy Package################################################
+https://garrettgman.github.io/tidying/
+#Prinzip: In Tabelle ist Information durch die Position eines Wertes codiert: x und y Position geben Auskunft über Beobachtung (Reihe) und Variable (Spalte), zu der der Wert gehört.
+##       country year  cases population
+## 1 Afghanistan 1999    745   19987071
+## 2 Afghanistan 2000   2666   20595360
+## 3      Brazil 1999  37737  172006362
+## 4      Brazil 2000  80488  174504898
+## 5       China 1999 212258 1272915272
+## 6       China 2000 213766 1280428583
+
+#Tabellen im Format Key-Value Pairs schreiben die Information über die y-Position in eine eigene Spalte. Insofern kann die Anzahl an Spalten reduziert werden, die Gesamtzahl an Zellen erhöht sich jedoch, da die Information, welche vorher in der Position codiert war, jetzt explizit in eigene Zellen geschrieben wird.
+##        country year        key      value
+## 1  Afghanistan 1999      cases        745
+## 2  Afghanistan 1999 population   19987071
+## 3  Afghanistan 2000      cases       2666
+## 4  Afghanistan 2000 population   20595360
+## 5       Brazil 1999      cases      37737
+## 6       Brazil 1999 population  172006362
+## 7       Brazil 2000      cases      80488
+## 8       Brazil 2000 population  174504898
+## 9        China 1999      cases     212258
+## 10       China 1999 population 1272915272
+## 11       China 2000      cases     213766
+## 12       China 2000 population 1280428583
+
+#Von Format 1 zu 2:
+gather(dataFrame, key, value, columns)#key: Name der Key Spalte (zB. "key") Value: Name der Value Spalte; columns: Spalten, die kombiniert werden sollen (zB. 3:4)
+
+#Von Format 2 zu 1:
+spread(dataFrame, key, value)#Erzeugt für jeden Faktor in Spalte key eine eigene Spalte und schreibt dort den jeweiligen Wert von value. Eventuell entstehende leere Zellen werden mit dem Argument von "fill=" gefüllt (Default: NA)
+
+#Data frame mit allen Kombinationen aus zwei oder mehr Vektoren
+expand.grid(v1,v2,v3,v4)
+
+
+########################Statistik###########################################
+#Standard deviation
+sd(vec)
+#Mean
+mean(vec)
+
+########################GGPlot2#############################################
+https://tutorials.iq.harvard.edu/R/Rgraphics/Rgraphics.html
+
+#Rotate labels (and adjust position)
+p1+theme(axis.text.x = element_text(angle = 90, hjust = 1))
+#Add labels
+p1+xlab("Label")
+p1+ggtitle("SettlemeyerA")
+p1+scale_colour_manual(values=c("#999999", "#E69F00", "red" ), name="ActTrans", breaks=c("X1", "X2","X3"),labels=c("1.0", "0.8","Sinusodial variations"))
+p1+labs(color = "Depth[cm]")#Colorbar caption
+#Set view (Axis limits)
+p1+coord_cartesian(xlim=c(0,100), ylim=c(0,100))
+#Invert Axis
+p1+scale_y_reverse(lim=c(10,0))
+
+#Plot Dates#library(scales)
+scale_x_date(date_breaks = "1 month", labels=date_format("%b-%Y"), limits = as.Date(c('2011-01-01','2013-01-01')))
+
+
+######Lineplots#######
+geom_line(aes(y=moisture, colour=Material, linetype=Types))
+geom_vline(xintercept=123, color="green", linetype="longdash")#create vertical line, horizontal: ...hline
+
+
+
+#########Barplots##############################
+#Balance: Drei Spalten: Name, Flow, Direction, wobei Direction nur In/Out (jeweils einmal für jeden Namen)
+p1=ggplot(Balance, aes(x=Name,y=Flow, fill=Direction))
+p1=p1+geom_bar(stat="identity", position=position_dodge())
+#dodge: Bewirkt, dass Balken für jede richtung nicht übereinander, sondern nebeneinander stehen (Vergleich von in/out möglich)
+#Identity: Explizite Angabe des y-werts jedes Balkens. Default: nur x-Wert wird angegeben (zB Name)=>Balken zeigen Häufigkeit jedes Faktors (jedes möglichen Namens)
+
+#################Arrange multiple plots###############
+grid.arrange(grobs=list(p1,p2,p3), top="Caption")#library(gridExtra), Aufruf evtl mit do.call("grid.arrange", Plotlist)
+
+#########################General language behaviour###############################
+#By reference or value: R in general: Always by value
+
+
+
+
+#################R Documentation################################
+roxygen: #Way to create documentation automatically
+#Create a package: https://tinyheero.github.io/jekyll/update/2015/07/26/making-your-first-R-package.html
+
+#Basic documentation of functions:
+#' HeaderLine
+#' @param param1: Description
+#' @param param2: Description
+#' @return Description
+#' @export
+func=function(param1, param2)
+
+
+#Create pdf Manual for packages: type in console ("R" stands short for R.exe executable)
+R CMD Rd2pdf mypackagename #command is based on Latex, so "pdflatex" must be available in PATH
+
+
